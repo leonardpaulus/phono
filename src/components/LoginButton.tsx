3963a38814f0fa6de9edbab5af2a1443d5fb3bb1
leonardpaulus <leonard.paulus@agenturignis.de>
2021-11-25 11:02:16 +0100

    💄 Use global variables on login button

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,7 +1,14 @@
 import styles from './LoginButton.module.css';
 
 function LoginButton(): JSX.Element {
-  return <button className={styles.loginButton}>Login with Discogs</button>;
+  return (
+    <a
+      className={styles.loginButton}
+      href={'http://localhost:3001/api/oauth/request_token'}
+    >
+      Login with Discogs
+    </a>
+  );
 }
 
 export default LoginButton;
