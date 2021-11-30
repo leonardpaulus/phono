dab66cf0145ff33dd22f615e85d3e1ffe381c6fb
leonardpaulus <leonard.paulus@agenturignis.de>
2021-11-30 14:35:42 +0100

    ✨ Add circles

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -5,6 +5,9 @@ import LoginButton from '../../components/LoginButton/LoginButton';
 export default function Login() {
   return (
     <div className={styles.loginPage}>
+      <div className={`${styles.outerCircle} ${styles.circle}`}></div>
+      <div className={`${styles.middleCircle} ${styles.circle}`}></div>
+      <div className={`${styles.innerCircle} ${styles.circle}`}></div>
       <div className={styles.components}>
         <Phono_Logo />
         <LoginButton />
