6fc59cf68a3974a966e1ec8fb62bf22a9a29d78b
leonardpaulus <leonard.paulus@agenturignis.de>
2021-12-01 16:00:27 +0100

    ✨ Add components to page

--- src/Pages/MyCollection/MyCollection.tsx
+++ src/Pages/MyCollection/MyCollection.tsx
@@ -1,10 +1,23 @@
 import styles from './MyCollection.module.css';
 import Phono_Logo from '../../assets/Phono_Logo';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import CoverSwiper from '../../components/CoverSwiper/CoverSwiper';
+import AlbumInfo from '../../components/AlbumInfo/AlbumInfo';
+import NavBar from '../../components/NavBar/NavBar';
 
 export default function MyCollection() {
   return (
     <div className={styles.myCollectionPage}>
       <Phono_Logo />
+      <SearchBar
+        placeholder={'Search my Collection'}
+        searchLocation={'myCollection'}
+      />
+      <CoverSwiper />
+      <AlbumInfo />
+      <div className={styles.navBar}>
+        <NavBar activeLink={'home'} />
+      </div>
     </div>
   );
 }
