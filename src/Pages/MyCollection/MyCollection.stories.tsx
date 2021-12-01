7801f336801250f30dc7915190cf991a5356732d
leonardpaulus <leonard.paulus@agenturignis.de>
2021-12-01 14:44:52 +0100

    ✨ Add my collection page

---
+++ src/Pages/MyCollection/MyCollection.stories.tsx
@@ -0,0 +1,8 @@
+import MyCollection from './MyCollection';
+
+export default {
+  component: MyCollection,
+  title: 'Pages/MyCollection',
+};
+
+export const LoginPage = () => <MyCollection />;
---
+++ src/Pages/MyCollection/MyCollection.tsx
@@ -0,0 +1,10 @@
+import styles from './MyCollection.module.css';
+import Phono_Logo from '../../assets/Phono_Logo';
+
+export default function MyCollection() {
+  return (
+    <div className={styles.myCollectionPage}>
+      <Phono_Logo />
+    </div>
+  );
+}
