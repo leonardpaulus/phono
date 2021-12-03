8ad4510265bb63017be5d08157a44526ddd5eef7
leonardpaulus <leonard.paulus@agenturignis.de>
2021-12-03 13:23:49 +0100

    ✨ Pass dynamic content in swiper & get index back

--- src/Pages/MyCollection/MyCollection.tsx
+++ src/Pages/MyCollection/MyCollection.tsx
@@ -1,11 +1,24 @@
 import styles from './MyCollection.module.css';
 import Phono_Logo from '../../assets/Phono_Logo';
 import SearchBar from '../../components/SearchBar/SearchBar';
-import CoverSwiper from '../../components/CoverSwiper/CoverSwiper';
 import AlbumInfo from '../../components/AlbumInfo/AlbumInfo';
 import NavBar from '../../components/NavBar/NavBar';
+import { useEffect, useState } from 'react';
+import useMyCollection from '../../utils/useMyCollection';
+import CoverSwiper from '../../components/CoverSwiper/CoverSwiper';
 
-export default function MyCollection() {
+export default function MyCollection(): JSX.Element {
+  const { getMyCollection, collection } = useMyCollection();
+  const [activeSlide, setActiveSlide] = useState<number>(0);
+
+  useEffect(() => {
+    getMyCollection();
+  }, []);
+
+  if (collection) {
+    console.log(collection[activeSlide]);
+  }
+
   return (
     <div className={styles.myCollectionPage}>
       <Phono_Logo />
@@ -13,8 +26,15 @@ export default function MyCollection() {
         placeholder={'Search my Collection'}
         searchLocation={'myCollection'}
       />
-      <CoverSwiper />
-      <AlbumInfo />
+      {collection && (
+        <CoverSwiper
+          collection={collection}
+          changeActiveSlide={(activeSlideIndex) =>
+            setActiveSlide(activeSlideIndex)
+          }
+        />
+      )}
+      {collection && <AlbumInfo />}
       <div className={styles.navBar}>
         <NavBar activeLink={'home'} />
       </div>
