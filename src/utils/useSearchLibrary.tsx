f0d9c008bae8200383b5ece95c9dd2d5b4408db9
leonardpaulus <leonard.paulus@agenturignis.de>
2021-12-04 17:11:27 +0100

    ✨ Add usesearchlibrary function

---
+++ src/utils/useSearchLibrary.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+export default function useSearchLibrary({ searchQuery }: string) {
+  const [searchResult, setSearchResult] = useState(null);
+
+  const encodedSearchQuery = encodeURIComponent(searchQuery);
+
+  const getSearchResult = async () => {
+    const response = await fetch(`/api/search/${encodedSearchQuery}`);
+    const search = await response.json();
+    setSearchResult(search);
+  };
+
+  return { getSearchResult, searchResult };
+}
