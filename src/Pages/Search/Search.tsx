69056b5f330f5a836cc437e7a48ff499d285cf5d
leonardpaulus <leonard.paulus@agenturignis.de>
2021-12-04 16:23:36 +0100

    ✨ Add search page

---
+++ src/Pages/Search/Search.tsx
@@ -0,0 +1,17 @@
+import Phono_Logo from '../../assets/Phono_Logo';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import NavBar from '../../components/NavBar/NavBar';
+import styles from './Search.module.css';
+
+export default function Search() {
+  return (
+    <div className={styles.searchPage}>
+      <Phono_Logo />
+      <SearchBar
+        placeholder={'Search the library'}
+        searchLocation={'myCollection'}
+      />
+      <NavBar activeLink={'search'} />
+    </div>
+  );
+}
