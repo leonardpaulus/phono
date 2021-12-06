9d6a1bfea1aad90ee9c482d23fec8ca3e26ca6fa
leonardpaulus <leonard.paulus@agenturignis.de>
2021-12-06 12:54:51 +0100

    💄 Style in collection with icon

--- src/components/SearchCard/SearchCardList.tsx
+++ src/components/SearchCard/SearchCardList.tsx
@@ -1,17 +1,29 @@
 import styles from './SearchCardList.module.css';
 import { SearchCardProps } from '../../lib/types';
+import InCollection from '../../assets/InCollection';
 
 export default function SearchCard({ searchResults }: SearchCardProps) {
   return (
     <>
       {searchResults.map((searchCard) => (
         <article className={styles.searchCard} key={searchCard.id}>
-          <img className={styles.albumCover} src={searchCard.cover} />
+          <div className={styles.albumCover__container}>
+            <img
+              className={
+                searchCard.in_collection
+                  ? `${styles.coverGreyscale} ${styles.albumCover}`
+                  : `${styles.albumCover}`
+              }
+              src={searchCard.cover}
+            />
+            {searchCard.in_collection && (
+              <div className={styles.inCollection}>
+                <InCollection />
+              </div>
+            )}
+          </div>
           <div className={styles.albumInfo}>
             <h2 className={styles.title}>{searchCard.title}</h2>
-            <p className={styles.inCollection}>
-              {searchCard.in_collection ? 'In Collection' : 'Not in Collection'}
-            </p>
           </div>
         </article>
       ))}
