ccd816de8e1833104a2c88a3289ea77c82bdb20b
leonardpaulus <leonard.paulus@agenturignis.de>
2021-12-06 09:37:45 +0100

    💚 Fix CI error

--- src/components/SearchCard/SearchCard.tsx
+++ src/components/SearchCard/SearchCard.tsx
@@ -2,15 +2,19 @@ import styles from './SearchCard.module.css';
 import { SearchCardProps } from '../../lib/types';
 
 export default function SearchCard({ searchResults }: SearchCardProps) {
-  return searchResults.map((searchCard, index) => (
-    <article className={styles.searchCard} key={index}>
-      <img className={styles.albumCover} src={searchCard.cover} />
-      <div className={styles.albumInfo}>
-        <h2 className={styles.title}>{searchCard.title}</h2>
-        <p className={styles.inCollection}>
-          {searchCard.in_collection ? 'In Collection' : 'Not in Collection'}
-        </p>
-      </div>
-    </article>
-  ));
+  return (
+    <>
+      {searchResults.map((searchCard, index) => (
+        <article className={styles.searchCard} key={index}>
+          <img className={styles.albumCover} src={searchCard.cover} />
+          <div className={styles.albumInfo}>
+            <h2 className={styles.title}>{searchCard.title}</h2>
+            <p className={styles.inCollection}>
+              {searchCard.in_collection ? 'In Collection' : 'Not in Collection'}
+            </p>
+          </div>
+        </article>
+      ))}
+    </>
+  );
 }
