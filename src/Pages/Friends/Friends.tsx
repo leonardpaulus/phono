f26e365dbeb25919e83bd70b93d2713a1ec17498
leonardpaulus <leonard.paulus@agenturignis.de>
2021-12-09 20:19:29 +0100

    ✨ Add useeffect to fetch friendslist

--- src/Pages/Friends/Friends.tsx
+++ src/Pages/Friends/Friends.tsx
@@ -2,8 +2,17 @@ import styles from './Friends.module.css';
 import Phono_Logo from '../../assets/Phono_Logo';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import NavBar from '../../components/NavBar/NavBar';
+import useFriends from '../../utils/useFriends';
+import { useEffect } from 'react';
 
 export default function Friends() {
+  const { getFriendsList, friendsList } = useFriends();
+
+  useEffect(() => {
+    getFriendsList();
+  }, []);
+  console.log(friendsList);
+
   return (
     <div className={styles.friendsPage}>
       <Phono_Logo />
