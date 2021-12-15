89d4a5e28198f0aa3011fd8ecd92dd737997ac70
leonardpaulus <leonard.paulus@agenturignis.de>
2021-12-15 20:32:43 +0100

    🎨 Remove overflow on login page & scale logo

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -5,9 +5,9 @@ import LoginButton from '../../components/LoginButton/LoginButton';
 export default function Login() {
   return (
     <div className={styles.loginPage}>
-      <div className={`${styles.outerCircle} ${styles.circle}`}></div>
-      <div className={`${styles.middleCircle} ${styles.circle}`}></div>
-      <div className={`${styles.innerCircle} ${styles.circle}`}></div>
+      <div className={`${styles.outerCircle} ${styles.circle}`} />
+      <div className={`${styles.middleCircle} ${styles.circle}`} />
+      <div className={`${styles.innerCircle} ${styles.circle}`} />
       <div className={styles.components}>
         <PhonoLogo />
         <LoginButton />
